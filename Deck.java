c6804f79581c84c948568ee42fb77bf0e5ef3946
CastorDev <egesezaki03@gmail.com>
2022-12-03 21:49:18 +0300

    added methods for creating and seeing the deck

--- Deck.java
+++ Deck.java
@@ -8,5 +8,30 @@ Clubs[3]: 1,2,3,4,5,6,7,8,9,10, Jack(11), queen(12), king(13)♣
 */
 
 public class Deck {
-Card[] deck = new Card[52];
+    private Card[] deck = new Card[52];
+    public void seeDeck(Card[] deck){
+        for(int i=0; i<deck.length;i++){
+            System.out.print(deck[i].getNumber()+deck[i].getSymbol()+" ,");//print i card number + symbol (untested)
+        }
+    }
+    public void createDeck(){//untested
+        for(int i = 0; i<deck.length;i++){
+            if(i<13){
+                deck[i].setNumber(i+1);
+                deck[i].setSymbol("♠");
+            }
+            else if(i<26){
+                deck[i].setNumber((i%13)+1);
+                deck[i].setSymbol("♥");
+            }
+            else if(i<39){
+                deck[i].setNumber((i%13)+1);
+                deck[i].setSymbol("♦");
+            }
+            else{
+                deck[i].setNumber((i%13)+1);
+                deck[i].setSymbol("♣");
+            }
+        }
+    }
 }
