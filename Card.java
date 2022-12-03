26f3da7febf02c336151a1d1b5dcff9355440f38
CastorDev <egesezaki03@gmail.com>
2022-12-03 21:20:42 +0300

    Created Card and Deck Classes
    Defined a card class with getters and setters
    Created deck class (placeholder for  now)

---
+++ Card.java
@@ -0,0 +1,9 @@
+//Card object
+public class Card {
+    private int number;
+    private String symbol;
+    public void setNumber(int a){number=0;}
+    public void setSymbol(String a){symbol=a;}
+    public int getNumber(){return number;}
+    public String getSymbol(){return symbol;}
+}
---
+++ Deck.java
@@ -0,0 +1,12 @@
+/*
+Deck:
+Spades[0]: 1,2,3,4,5,6,7,8,9,10, Jack(11), queen(12), king(13)♠
+Hears[1]: 1,2,3,4,5,6,7,8,9,10, Jack(11), queen(12), king(13)♥
+Diamonds[2]: 1,2,3,4,5,6,7,8,9,10, Jack(11), queen(12), king(13)♦
+Clubs[3]: 1,2,3,4,5,6,7,8,9,10, Jack(11), queen(12), king(13)♣
+52 cards
+*/
+
+public class Deck {
+Card[] deck = new Card[52];
+}
