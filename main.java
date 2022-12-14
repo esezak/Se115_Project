74f2e7828c7bdf0672a80da329311361576253d0
CastorDev <egesezaki03@gmail.com>
2022-12-14 20:33:55 +0300

    Added Player turn system and input verification
    Also added comments for future plans

--- main.java
+++ main.java
@@ -1,3 +1,4 @@
+import java.time.format.SignStyle;
 import java.util.Scanner;
 public class main {
     /*---------------------Static functions-------------------------*/
@@ -5,17 +6,22 @@ public class main {
         board.addToBoard(hand.getCard(index));
         hand.removeCard(index);
     }
+    /*--------------------------------------------------------------*/
     /*---------------------Start of the game------------------------*/
     public static void main(String[] args){
+
         /*--------------Classes and variables------------------*/
+
         Deck deck = new Deck();
         Hand player = new Hand();
         Hand enemy = new Hand();
         Board board = new Board();
         Scanner sc = new Scanner(System.in);
-        String input="";
-        boolean okInput = true;
+        String input;
+        boolean okInput = true;// boolean to verify if a user input is verified
+        int turnkeeper =0;//to keep track of which turn we are (to deal new cards)
         final String error ="Entered a faulty input try again ";
+
         /*-------------Classes and variables end---------------*/
 
 
@@ -23,7 +29,9 @@ public class main {
         deck.create();
         deck.shuffle();
         /*------------------------------------------------------*/
+
         /*---------------------Game Start-----------------------*/
+
         System.out.println("Welcome to Pişti!");
         System.out.print("Select where you want to cut the deck: ");
         
@@ -37,41 +45,68 @@ public class main {
                 System.out.print(error + "(0-52): ");
             }
         }okInput = true;
+        // 10 lines and 3 indents for executing a single function that require user input
+        // I think I am starting to understand why people do not like Java
         /*------------------------------------------*/
 
-        deck.cut(26);
-        System.out.println();
-        deck.see();
-        player.fillHand(deck, enemy);
-        System.out.println();
-        player.see();
-        System.out.println();
-        enemy.see();
-        System.out.println("\n"+deck.seeTopcard()+"\n");
+        player.fillHand(deck, enemy);// deal the cards
         board.startBoard(deck);
-        board.seeBoard();
-        System.out.println("\n"+deck.seeTopcard()+"\n");
-        deck.see();
-        System.out.print("\nPlay a card: ");
+        System.out.println();System.out.println();
 
+        while(deck.getTopcard()<52){//loop until cards are finished
 
-        while(okInput) {
-            try {
-                input = sc.nextLine();
-                playCard(Integer.parseInt(input) - 1, player, board);
-                okInput = false;
-            } catch (Exception e) {
-                System.out.print(error+" (1-4): ");
-            }
+            System.out.print("Board: ");
+            board.seeBoard();
+            System.out.println("\nChoose a card to play 1 to 4:");
+            player.see();System.out.println();
+
+            while(okInput) {//place card on board
+                try {
+                    input = sc.nextLine();
+                    //check if selected place at hand is empty
+                    if(!player.getCard(Integer.parseInt(input)-1).getCard().equals("00")) {
+                        playCard(Integer.parseInt(input) - 1, player, board);
+                        okInput = false;
+                    }
+                    else{
+                        System.out.print("This spot is empty try again: ");
+                    }
+                } catch (Exception e) {
+                    System.out.print(error+" (1-4): ");
+                }
+            }okInput = true;
+
+            /*------------------TO-DO------------------------*/
+            //check if the player takes the cards or is there a pişti
+
+            //computer plays a card
+
+            //check if the copmuter takes the cars or is there a pişti
+
         }
-        board.seeBoard();
-        System.out.println();
-        player.see();
+
+    //calculate points
+
+    //Check who won
+
+    //Check the scores on the top 10 list if the player managed to get in the list
+
+    //Print the list to player to see
+
+    //Game finish
+
+
+
     }
     /*----------------------End of the Game-----------------------------------*/
 
     /*-----------------------Ai Code Here-------------------------------*/
+/*
+Basic functions:
+1- if top card on board in hand play card
+2- if not play a random valid card in deck
 
+ */
 
     /*-----------------------Ai Code end here---------------------------*/
 }
