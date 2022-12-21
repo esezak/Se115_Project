43920c897439ed956f9e728d7eff198e3c28a23a
CastorDev <egesezaki03@gmail.com>
2022-12-21 20:11:40 +0300

    overhauled point calculations

--- Board.java
+++ Board.java
@@ -1,6 +1,7 @@
 public class Board {
-    private Card[] board = new Card[52];
-
+    private Card[] board = new Card[55];
+    // 55 card "number" actually means points
+    //since few cards have extra points they are added with "dead space" on board (pun not intended)
     private int topindex=0;
     public void startBoard(Deck deck){
         Card empty = new Card();
@@ -26,9 +27,26 @@ public class Board {
         }
     }
     public void addToBoard(Card newCard){
-        topindex++;
+        if(newCard.getCard().equals("10♦")){
+            topindex +=3;
+        }else if(newCard.getCard().equals("2♣")){
+            topindex +=2;
+        }else{
+            topindex++;
+        }
         board[topindex]=newCard;
     }
+    public int countCard(){// basicaly seeBoard but it gives the number of cards
+        int counter =0;
+        for(int i=0;i< board.length;i++){
+            if(!board[i].getCard().equals("00")) {
+                counter++;
+            }
+        }
+        return counter;
+    }
+
+
     public void flushBoard(){
         Card empty = new Card();
         empty.setSymbol("0");
